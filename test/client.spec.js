9b9e093dfdac1290cf75fc4d0061f98e803fea6b
Osho Klinsmann <oshoklinsmann@gmail.com>
2017-11-18 06:32:04 +0100

    list orders method completed and tested

--- test/client.spec.js
+++ test/client.spec.js
@@ -4,17 +4,17 @@ const chai = require("chai");
 const expect = chai.expect;
 const client = require("../lib/client");
 const config = require("../lib/configure");
-const defaultOptions = require("../config/configure").defaultOptions;
+
+var options = {
+    api_key: 'etQE8xLjP3XVJZuUDfodK2',
+    api_secret: 'm1nrbhCDioGIZ9PQl4jSRXL7OYuMKtVs',
+    app_id: '705',
+    mode: 'sandbox'
+};
+config.configure(options);
 
 describe.skip('Client - Creating Order', function () {
    it('should return status 200 and payment url', function (done) {
-       var options = {
-           api_key: 'API-KEY',
-           api_secret: 'API-SECRET',
-           app_id: 'APP-ID',
-           mode: 'sandbox'
-       };
-       
        var data = {
            order_id:         'ORDER-1412759367',
            price:            1050.99,
@@ -25,11 +25,37 @@ describe.skip('Client - Creating Order', function () {
            success_url:      'https://example.com/account/orders',
            description:      'Apple Iphone 6'
        };
-       config.configure(options);
-       console.log('new options', defaultOptions)
        client.order(data, function (err, res, body) {
            expect(res).property('statusCode').to.equal(200);
+           expect(body).property('id').to.be.finite;
            done();
        });
    });
+});
+
+describe.skip('Client - Get Order Details', function () {
+    it('should return status 200 and order details', function (done) {
+        var orderID = '13245';
+        client.getOrder(orderID, function (err, res, body) {
+            expect(res).property('statusCode').to.equal(200);
+            expect(body).to.have.property('price');
+            expect(body).to.have.property('status');
+            done();
+        })
+    })
+});
+
+describe.skip('Client - Get All Orders', function () {
+    it('should return status 200 and all orders', function (done) {
+        var data = {
+            per_page: 50,
+            page: 1,
+            sort: 'created_at_desc'
+        };
+        client.listOrders(data, function (err, res, body) {
+            expect(res).property('statusCode').to.equal(200);
+            expect(body).to.have.property('orders');
+            done();
+        })
+    });
 });
